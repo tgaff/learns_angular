f826be5ae83736106e8229f941d8a78a17ab12c7
tgaff <tgaff@alumni.nd.edu>
2015-11-07 13:09:31 -0800

    localstorage attached

--- day_1/todo_html/app.js
+++ day_1/todo_html/app.js
@@ -3,11 +3,20 @@ var app = angular.module("todoApp", []);
 app.controller("todosController", function($scope) {
   $scope.test = 'angular connected';
 
-  $scope.todos = [
-    { body: 'I want to go shopping', status: 'completed'},
+  $scope.initialize = function() {
+    var stored = JSON.parse(localStorage.getItem('todos'));
+    if (stored) {
+      $scope.todos = stored;
+    }else {
+      $scope.todos = [
+        { body: 'I want to go shopping', status: 'completed'},
 
-    { body: 'Clean the house', status: 'incomplete' }
-  ];
+        { body: 'Clean the house', status: 'incomplete' }
+      ];
+    }
+  };
+
+  $scope.todos = [];
 
   $scope.toggleComplete = function(todo) {
     if (todo.status === 'completed') {
@@ -15,16 +24,30 @@ app.controller("todosController", function($scope) {
     }else {
       todo.status = 'completed';
     }
+    $scope.persist();
   };
 
   $scope.deleteTodo = function(todo) {
     console.log('delete');
     var index = $scope.todos.indexOf(todo);
     $scope.todos.splice(index, 1);
+    $scope.persist();
   };
 
   $scope.addTodo = function() {
     $scope.todos.push( { body: $scope.newTodoInput, status: 'incomplete' } );
+    $scope.persist();
   }
+
+  $scope.persist = function() {
+    console.log('SAVE');
+    localStorage.clear();
+    console.log($scope.todos);
+    localStorage.setItem('todos', angular.toJson($scope.todos));
+  };
+
+
+  $scope.initialize();
+  $scope.persist();
 });
 
