253edb8b69a02c5556c5e58f6d99849dc0af5165
tgaff <tgaff@alumni.nd.edu>
2015-11-08 15:58:34 -0800

    Setup angular to search for movie and show the correct movie when found using directives

--- day_2/movie_starter_app/js/app.js
+++ day_2/movie_starter_app/js/app.js
@@ -1,1 +1,42 @@
-//Your code here
+//http://omdbapi.com/?t=frozen
+//
+var app = angular.module("movieApp", []);
+
+
+app.directive("moviesDirective", function() {
+  console.log('directive');
+  return {
+    templateUrl: 'templates/movies.html',
+    controller: 'moviesCtrl',
+    restrict: 'A' // A = attribute , E = element, C = class
+  };
+});
+
+
+
+
+app.controller('moviesCtrl', function($scope, $http) {
+  console.log('yay controller');
+  $scope.movie = {};
+
+  var fetchMovie = function(search) {
+    $http.get('http://omdbapi.com/?t=' + search)
+    .success(function(movie) {
+       $scope.movie = movie;
+       console.log('got ', movie);
+       // showMovie;
+       console.log('has property title? ', $scope.movie.hasOwnProperty('Title'));
+    })
+    .error(function() {
+      alert('couldnt find movie ' + search);
+    });
+  };
+
+  $scope.search  = function() {
+    console.log('searching for ', $scope.searchQuery);
+    //hideSearch();
+    fetchMovie($scope.searchQuery);
+  };
+
+
+});
