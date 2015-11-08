95a034538a28f3169ebce17c643744d4452e721f
tgaff <tgaff@alumni.nd.edu>
2015-11-08 11:38:27 -0800

    add animation to wine manager

--- day_1/wine_manager_html/app.js
+++ day_1/wine_manager_html/app.js
@@ -1,7 +1,25 @@
-var app = angular.module("wineManager", []);
+var app = angular.module("wineManager", ['ngRoute', 'ngAnimate']);
+
+app.config(["$routeProvider", function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "templates/index.html",
+            controller: "winesController"
+        })
+        .when("/edit/:id", {
+          templateUrl: "templates/edit.html",
+          controller: 'wineController'
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+}]);
+
+
+
 
 app.controller("winesController", function($scope, $http) {
-
+  console.log('ok');
   var updateWines = function() {
     $http.get('http://daretodiscover.herokuapp.com/wines')
       .success(function(data) {
@@ -29,4 +47,36 @@ app.controller("winesController", function($scope, $http) {
   // on initialization
   updateWines();
 
+});
+
+
+app.controller("wineController", function($scope, $http, $routeParams, $location) {
+
+  $scope.wineID = $routeParams.id;
+  var getWine = function() {
+    console.log('getting wine ', $scope.wineID);
+    $http.get('http://daretodiscover.herokuapp.com/wines/' + $scope.wineID)
+      .success(function(wine) {
+        $scope.wine = wine;
+      })
+      .error(function(data) {
+        alert('failed to get wine with id ', $scope.wineID);
+      });
+  };
+
+  $scope.updateWine = function() {
+    console.log('updateWine()');
+    $http.put('http://daretodiscover.herokuapp.com/wines/' + $scope.wineID, $scope.wine)
+      .success(function(wine) {
+        console.log('it worked');
+        $location.path('/');
+
+      })
+      .error(function() {
+        alert('failed to save');
+      });
+  };
+
+
+  getWine();
 });
